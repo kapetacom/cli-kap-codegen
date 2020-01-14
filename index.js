4a216089126c889e7bcfe5a9bdbfe9dac2f178ba
Henrik von Hofmeister <hh@cloudscrape.com>
2020-01-14 21:32:50 +0100

    Load targest from config

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const ClusterConfiguration = require('@blockware/local-cluster-config');
 const {registry:Targets} = require('@blockware/codegen');
 const packageData = require('./package');
 
-const providerDir = ClusterConfiguration.getProvidersBasedir();
+const TARGET_KIND = 'core.blockware.com/v1/LanguageTarget';
+const languageTargets = ClusterConfiguration.getProviderDefinitions(TARGET_KIND);
+languageTargets.forEach((languageTarget) => {
+    Targets.register(languageTarget.definition.metadata.id, require(languageTarget.path));
+});
 
-Targets.load(providerDir);
 
 const command = new BlockwareCommand('codegen', packageData.version);
 command.program()
